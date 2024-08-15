17df32e8109f686e281a5ff515cb4b24ef9c3f8d
PC 6 <>
2024-08-15 17:33:39 -0600

    actualizacionfunciones

--- prueba.js
+++ prueba.js
@@ -1,1 +1,18 @@
-"Ejercico prueba";
+const inputOperaciones = document.querySelector("input");
+
+function actualizarVentana(boton) {
+    if (boton.innerHTML === "=") {
+        inputOperaciones.value = eval(inputOperaciones.value);
+    } else {
+        inputOperaciones.value += boton.innerHTML;
+    }
+
+}
+
+function limpiarPantalla() {
+    if (boton.innerHTML === "C") {
+        
+        
+    }
+    
+}
