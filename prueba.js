a3a590a0c18aa34d4457a7e1c6ddccc0cf32b1e9
eduardo <e.amoresano85@gmail.com>
2024-08-21 21:24:19 -0300

    avanceCalculadora

--- prueba.js
+++ prueba.js
@@ -1,25 +1,33 @@
 const inputOperaciones = document.querySelector("input");
 
-function actualizarVentana(boton) {
-    if (boton.innerHTML === "=") {
-        inputOperaciones.value = eval(inputOperaciones.value);
-    } else {
-        inputOperaciones.value += boton.innerHTML;
-    }
+const OPCION_RESULTADO = "=";
+const OPCION_LIMPIAR = "C";
+const OPCION_BORRAR_ULTIMO = "←";
+const PANTALLA_VACIA = "";
+const OPERADORES = "+-*/";
+const NUMEROS = "0123456789";
 
+function actualizarVentana(boton) {
+    let ultimoCaracter = inputOperaciones.value[inputOperaciones.value.length-1];
+    if (!OPERADORES.includes(ultimoCaracter)) || (NUMEROS.includes(boton.innerHTML)) {
+        switch (boton.innerHTML) {
+            case OPCION_RESULTADO:
+                inputOperaciones.value = eval(inputOperaciones.value);
+                break;
+            case OPCION_LIMPIAR:
+                inputOperaciones.value = PANTALLA_VACIA;
+                break;
+            case OPCION_BORRAR_ULTIMO:
+                inputOperaciones.value = inputOperaciones.value.slice(0,-1);
+                break;
+            default:
+                inputOperaciones.value += boton.innerHTML;    
+                break;
+        }  
+    }else{
+        alert("opcion no permitida")
+    } 
 }
 
-function limpiarPantalla() {
-        inputOperaciones.value = "";
-    }
-    
-function borrarUltimo() {
-        inputOperaciones.value = inputOperaciones.value.slice(0,-1);
-    }
-
-function evitarDuplicado() {
-
-}    
-    
 
    
